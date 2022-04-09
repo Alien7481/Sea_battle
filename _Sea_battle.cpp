442423f86b23d9fc3e8174e947e0eb98ddc5c554
Nastya <post@post.post>
2022-04-09 20:18:42 +0500

    The first commit
    Printin 10*10 field

---
+++ _Sea_battle.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+
+    int size = 10; 
+
+    for (int i = 0; i < size+2; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == 0)
+                cout << "___";
+            if (i == size +2)
+                    cout << "---";
+            if (i >0 || i< size + 2)
+                cout << setw(3) << "|";
+          
+            
+        }
+        cout << "\n";
+        
+
+    }
+        
+
+        cout << "\n";
+}
+
+    
